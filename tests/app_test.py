fa26c824411566cde07180ddc31adb69092b2496
Sean <seanpatrickllera@yahoo.ca>
2022-11-16 19:45:02 -0500

    Implemented Search Page

--- tests/app_test.py
+++ tests/app_test.py
@@ -75,6 +75,25 @@ def test_messages(client):
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
 
+def test_search(client):
+    """Ensure that user can search messages"""
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.post(
+        "/add",
+        data=dict(title="<Hello>", text="<strong>HTML</strong> allowed here"),
+        follow_redirects=True,
+    )
+    rv = client.post(
+        "/add",
+        data=dict(title="Yo", text="This is a new test"),
+        follow_redirects=True,
+    )
+    rv = client.get('/search/?query=yo')
+    assert b"Yo" in rv.data
+    assert b"This is a new test" in rv.data
+    assert b"&lt;Hello&gt;" not in rv.data
+    assert b"<strong>HTML</strong> allowed here" not in rv.data
+
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
     rv = client.get('/delete/1')
